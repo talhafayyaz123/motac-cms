d568c0c10163a1c9d34b71c0d495e941a847850b
khurram <khurram@paynest.ae>
2024-09-13 21:16:04 +0500

    fix(fixed dashboard bugs): fixed dashboard dropdown design bug area chart design bug

--- src/components/ui/dataTable/Select.tsx
+++ src/components/ui/dataTable/Select.tsx
@@ -20,10 +20,17 @@ const Select: React.FC<SelectProps> = ({
       <select
         value={value}
         onChange={onChange}
-        className={`w-full px-4 py-2 text-sm rounded-md cursor-pointer transition-all appearance-none focus:outline-none focus:ring-2 focus:ring-offset-2
-          ${value === highlightValue ? 'bg-blue-100 text-white ring-2 ring-blue-100 ring-offset-0' : 'bg-white'}
-          ${minimalStyle ? 'border border-gray-300 text-gray-700 py-2 pl-3 pr-8 focus:ring-blue-500' : 'ring-1 ring-gray-500 ring-offset-2'}
-        `}
+        className={`${
+          minimalStyle
+            ? 'border border-gray-300 text-gray-700 py-2 pl-3 pr-8 focus:ring-blue-500'
+            : 'ring-1 ring-gray-500 ring-offset-2'
+        } 
+    px-4 py-1 text-sm rounded-md outline-none ${
+      value === highlightValue
+        ? 'bg-blue-100 text-white ring-2 ring-blue-100 ring-offset-0'
+        : 'bg-white'
+    }`}
+        style={minimalStyle ? { appearance: 'none' } : undefined}
       >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
@@ -31,7 +38,9 @@ const Select: React.FC<SelectProps> = ({
           </option>
         ))}
       </select>
-      <span className="absolute inset-y-0 right-[-10px] flex items-center pr-4 pointer-events-none">
+      <span
+        className={`absolute inset-y-0 right-[-10px] flex items-center pr-4 pointer-events-none ${!minimalStyle ? 'hidden' : 'block'}`}
+      >
         <svg
           className="w-4 h-4 text-gray-500"
           fill="none"
